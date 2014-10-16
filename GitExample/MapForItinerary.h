6254ef2d48f0b8aaf72d00565c4c6bffc6aa3934
sureshediga <sediga@pasonatquila.com>
2014-10-16 15:58:32 +0900

    Map function

---
+++ GitExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitExample
+//
+//  Created by SureshGoud on 2014/10/16.
+//  Copyright (c) 2014年 Suresh Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
